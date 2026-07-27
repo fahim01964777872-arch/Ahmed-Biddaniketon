156a2032f92c261222103696cb38df17f5823212
fahim01964777872-arch <fahim01964777872@gmail.com>
2026-07-28 00:52:20 +0600

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,26 @@
+// Firebase SDK Imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
 
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB-ON23dzRy57s1ZfpLEIpM4wyRN8oUN0w",
+  authDomain: "ahmed-biddaniketon.firebaseapp.com",
+  projectId: "ahmed-biddaniketon",
+  storageBucket: "ahmed-biddaniketon.firebasestorage.app",
+  messagingSenderId: "1092422968703",
+  appId: "1:1092422968703:web:b0f24f04521a45ed47a64a",
+  measurementId: "G-ECM2RB1E3M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Services
+const db = getFirestore(app);
+const storage = getStorage(app);
+const auth = getAuth(app);
+
+export { app, db, storage, auth };
